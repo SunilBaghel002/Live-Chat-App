119fad28deeb486aab86d06548d5d5405005a995
SunilBaghel002 <sunilbaghel93100@gmail.com>
2025-03-10 11:44:49 +0530

    improve

--- server.js
+++ server.js
@@ -24,9 +24,9 @@
 //     console.log("Server running on http://localhost:3000");
 // });
 // server.js
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -35,21 +35,23 @@ const io = socketIo(server);
 // Serve static files from the 'public' directory
 // app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public")));
-
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 io.on("connection", (socket) => {
-    console.log("New user connected");
+  console.log("New user connected");
 
-    // Listen for incoming messages and broadcast to all connected clients
-    socket.on("newMessage", (data) => {
-        // data is an object { username, message, timestamp }
-        io.emit("newMessage", data);
-    });
+  // Listen for incoming messages and broadcast to all connected clients
+  socket.on("newMessage", (data) => {
+    // data is an object { username, message, timestamp }
+    io.emit("newMessage", data);
+  });
 
-    socket.on("disconnect", () => {
-        console.log("User disconnected");
-    });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
 server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+  console.log("Server running on http://localhost:3000");
 });
