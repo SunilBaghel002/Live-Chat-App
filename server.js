8bc51a51265a9855964fd5198e3c1e62e8396488
SunilBaghel002 <sunilbaghel93100@gmail.com>
2025-03-10 11:54:14 +0530

    views folder

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-// const express = require("express");
-// const http = require("http");
-// const socketIo = require("socket.io");
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server);
-
-// app.use(express.static("public")); // Serve frontend files
-
-// io.on("connection", (socket) => {
-//     console.log("New user connected");
-
-//     socket.on("newMessage", (message) => {
-//         io.emit("newMessage", message); // Broadcast to all users
-//     });
-
-//     socket.on("disconnect", () => {
-//         console.log("User disconnected");
-//     });
-// });
-
-// server.listen(3000, () => {
-//     console.log("Server running on http://localhost:3000");
-// });
-// server.js
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
@@ -32,13 +6,12 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the 'public' directory
-// app.use(express.static("public"));
-// app.use(express.static(path.join(__dirname, "public")));
-app.set("public", path.join(__dirname, "public"));
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "index.html"))
+  res.sendFile(path.join(__dirname, "/views/index.html"))
 );
+
 io.on("connection", (socket) => {
   console.log("New user connected");
 
